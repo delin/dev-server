3219b385ececf1832b162e060eec806ce3fdc067
Yulin Denis <delin@eridan.la>
2012-05-07 23:00:22 +0400

    web ui v0.1 доработан

--- web/main.py
+++ web/main.py
@@ -21,7 +21,10 @@
 #  MA 02110-1301, USA.
 
 
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, jsonify
+from time import sleep, ctime
+from os import uname, getloadavg
+from psutil import phymem_usage, used_phymem, cached_phymem, virtmem_usage, cpu_percent, disk_usage, get_pid_list
 
 
 app = Flask(__name__)
@@ -30,6 +33,59 @@ app.config.from_envvar('DEV-SERVER_SETTINGS', silent = True)
 app.secret_key = 'bb5d 580;$ 2 b8cfc6a2 …'
 DEBUG = True
 
+def system_uptime():
+	try:
+		f = open( "/proc/uptime" )
+		contents = f.read().split()
+		f.close()
+	except Exception, ex:
+		print "Cannot open uptime file: /proc/uptime", ex
+		return False
+
+	total_seconds = float(contents[0])
+
+	# Helper vars:
+	MINUTE	= 60
+	HOUR	= MINUTE * 60
+	DAY	= HOUR * 24
+
+	# Get the days, hours, etc:
+	days	= int( total_seconds / DAY )
+	hours	= int( ( total_seconds % DAY ) / HOUR )
+	minutes = int( ( total_seconds % HOUR ) / MINUTE )
+	seconds = int( total_seconds % MINUTE )
+
+	# Build up the pretty string (like this: "N days, N hours, N minutes, N seconds")
+	string = ""
+	if days > 0:
+		string += str(days) + " " + (days == 1 and "day" or "days" ) + ", "
+	if len(string) > 0 or hours > 0:
+		string += (hours < 10 and "0" + str(hours) or str(hours)) + ":"
+	if len(string) > 0 or minutes > 0:
+		string += (minutes < 10 and "0" + str(minutes) or str(minutes)) + ":"
+
+	string += (seconds < 10 and "0" + str(seconds) or str(seconds))
+
+	return string
+
+@app.route('/ajax/sys_stat.json')
+def ajax_stat():
+	sys_stats = dict(
+		load_avarage	= getloadavg(),
+		date		= ctime(),
+		uptime		= system_uptime(),
+		cpu_percent	= cpu_percent(interval = 0),
+		mem_percent	= phymem_usage().percent,
+		mem_usage	= (used_phymem() - cached_phymem()) / 1024 / 1024,
+		swap_percent	= virtmem_usage().percent,
+		swap_usage	= virtmem_usage().used / 1024 / 1024,
+		disk_percent	= disk_usage('/home').percent,
+		disk_usage	= disk_usage('/home').used / 1024 / 1024 / 1024,
+		disk_total	= disk_usage('/home').total / 1024 / 1024 / 1024,
+		procs_total	= len(get_pid_list())
+	)
+
+	return jsonify(sys_stats = sys_stats)
 
 @app.route("/")
 def page_index():
@@ -37,7 +93,12 @@ def page_index():
 	url_for('static', filename = 'img/*.png')
 	url_for('static', filename = 'js/*.js')
 
-	return render_template('pages/index.html')
+	u = uname()
+	sys_stats = dict(
+		host_info = u[1] + ": " + u[0] + "-" + u[2] + "-" + u[4] +": "
+	)
+
+	return render_template('pages/index.html', sys_stats = sys_stats)
 
 if __name__ == "__main__":
 	app.run(host = '0.0.0.0', port = 8081, debug = DEBUG)
--- web/static/js/application.js
+++ web/static/js/application.js
@@ -1,3 +1,47 @@
+$(document).ready(function() {
+  $('#modal-web_off').modal({
+    keyboard: false,
+    show: false
+  });
+
+  setInterval(function()
+  {
+    var jqxhr = $.getJSON('/ajax/sys_stat.json',
+      function(data) {
+        $('#modal-web_off').modal('hide');
+
+        $("#sys_stat-load_avarage").text(data.sys_stats.load_avarage[0] + ", " + data.sys_stats.load_avarage[1] + ", " + data.sys_stats.load_avarage[2]);
+        $("#sys_stat-date").text(data.sys_stats.date);
+        $("#sys_stat-uptime").text(data.sys_stats.uptime);
+
+        red = (Math.round(255 * data.sys_stats.cpu_percent / 100)).toString(16);
+        green = (Math.round(255 * (100 - data.sys_stats.cpu_percent) / 100)).toString(16);
+        $("#sys_stat-cpu_percent").css('background-color', "#" + red + green + "00");
+
+        red = (Math.round(255 * data.sys_stats.mem_percent / 100)).toString(16);
+        green = (Math.round(255 * (100 - data.sys_stats.mem_percent) / 100)).toString(16);
+        $("#sys_stat-mem_percent").css('background-color', "#" + red + green + "00");
+
+        red = (Math.round(255 * data.sys_stats.disk_percent / 100)).toString(16);
+        green = (Math.round(255 * (100 - data.sys_stats.disk_percent) / 100)).toString(16);
+        $("#sys_stat-disk_percent").css('background-color', "#" + red + green + "00");
+
+        $("#sys_stat-cpu_percent").css('width', data.sys_stats.cpu_percent + '%');
+        $("#sys_stat-cpu_percent-label").text(data.sys_stats.cpu_percent + '%');
+        $("#sys_stat-mem_usage").text(data.sys_stats.mem_usage + "Mb");
+        $("#sys_stat-mem_percent").css('width', data.sys_stats.mem_percent + '%');
+        $("#sys_stat-swap_usage").text(data.sys_stats.swap_usage + "Mb");
+        $("#sys_stat-swap_percent").css('width', data.sys_stats.swap_percent + '%');
+        $("#sys_stat-disk_usage").text(data.sys_stats.disk_usage + "/" + data.sys_stats.disk_total + " Gb");
+        $("#sys_stat-disk_percent").css('width', data.sys_stats.disk_percent + '%');
+        $("#sys_stat-procs_total").text(data.sys_stats.procs_total);
+      })
+    .error(function() {
+      $('#modal-web_off').modal('show');
+    })
+  }, 2000);
+});
+
 //~ // button state demo
 //~ $('#fat-btn-srv1-stop')
   //~ .click(function () {
